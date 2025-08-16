207adf81d298b42ef51c5c734525105efce69bfc
Alex Levy <ajlevy246@gmail.com>
2025-08-16 02:05:46 -0400

    removed

--- src/blocks/Sections/Education.tsx
+++
@@ -1,8 +0,0 @@
-export default function Education() {
-    return (
-        <section id="education" className="pt-[25%] md:pt-[8%]">
-            <div id="education" className="sectionTitle text-center text-5xl">Education</div>
-            <p className="h-screen">Welcome welcome welcome</p>
-        </section>
-    )
-}
