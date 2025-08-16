6074ce00697f79cafcaa8960eff04cc6b6d76461
Alex Levy <ajlevy246@gmail.com>
2025-08-16 02:04:11 -0400

    mobile style changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import HeroIntro from "./hero";
 import About from "@/blocks/Sections/About";
 import RollingGallery from "@/blocks/Components/RollingGallery/RollingGallery";
-import Education from "@/blocks/Sections/Education";
+import Education from "@/blocks/Sections/Education/Education";
 import Projects from "@/blocks/Sections/Projects";
 
 export default function Home() {
@@ -11,7 +11,7 @@ export default function Home() {
       <HeroIntro />
       <section className="gradBackground px-[10%]">
       <About />
-      <div id="gallery" className="mt-[10%]">
+      <div id="gallery" className="hidden sm:flex sm:mt-[10%]">
       <RollingGallery
         autoplay={true}
         pauseOnHover={false}
