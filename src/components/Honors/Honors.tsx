e74a328fc075d9bbf835317bce5a48d0f06e482c
Alex Levy <ajlevy246@gmail.com>
2025-08-21 13:02:34 -0400

    added Honors and Awards section

---
+++ src/components/Honors/Honors.tsx
@@ -0,0 +1,12 @@
+export default function Honors() {
+    return (
+        <section id="honors" className='pt-[25%] md:pt-[8%]'>
+            <div className="sectionTitle text-center text-5xl">Honors and Awards</div>
+            <section>
+                <div>The Clifford Beck Award - Montgomery College, 2023</div>
+                <div>Eagle Scout - Troop 457 Rockville, MD, 2023</div>
+                <div>AP Scholar with Distinction, College Board, 2021</div>
+            </section>
+        </section>
+    )
+}
