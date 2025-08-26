c7f0b1e6f0d839cf2f20d63b83c8f5d053fbae31
ajlevy246 <ajlevy246@gmail.com>
2025-08-26 19:57:04 -0400

    added content and styling

--- src/components/Honors/Honors.tsx
+++ src/components/Honors/Honors.tsx
@@ -1,25 +1,44 @@
+'use client';
+
+import { motion } from "motion/react";
+
+import "./honors.css";
+
 export default function Honors() {
     return (
         <section id="honors" className="pt-[25%] md:pt-[8%] text-center">
-            {/* Section Title */}
-            <h2 className="sectionTitle text-5xl ">
+            {/* Section Title
+            <h2 className="sectionTitle text-5xl mb-[50px]">
                 Honors and Awards
-            </h2>
+            </h2> */}
 
             {/* Honors List */}
-            <div><b>The Clifford Beck Award</b> - Montgomery College, 2023</div>
-            <div><b>Eagle Scout</b> - Troop 457, Rockville, MD, 2023</div>
-            <div><b>AP Scholar with Distinction</b> - College Board, 2021</div>
-            <div className="space-y-4 max-w-xl mx-auto text-gray-300 text-lg leading-relaxed">
-                <div className="hover:text-white transition-colors duration-300">
-                    <span className="font-bold">The Clifford Beck Award</span> - Montgomery College, 2023
-                </div>
-                <div className="hover:text-white transition-colors duration-300">
-                    <span className="font-bold">Eagle Scout</span> - Troop 457 Rockville, MD, 2023
-                </div>
-                <div className="hover:text-white transition-colors duration-300">
-                    <span className="font-bold">AP Scholar with Distinction</span>, College Board, 2021
-                </div>
+            <div>
+                {/* Honors/Awards */}
+                <motion.ul
+                    initial={{ opacity: 0, x: -65 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    viewport={{ once: true }}
+                    transition={{ duration: 1, ease: "easeOut" }}
+                >
+                    <h1>Honors</h1>  
+                    <li><b>The Clifford Beck Award for Excellency in Physics</b> - <i>Montgomery College, 2023</i></li>
+                    <li><b>Eagle Scout</b> - <i>Troop 457 Rockville, MD, 2023</i></li>
+                    <li><b>AP Scholar with Distinction</b> - <i>College Board, 2021</i></li>
+                </motion.ul>
+
+                {/* Scholarships */}
+               <motion.ul
+                    initial={{ opacity: 0, x: 65 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    viewport={{ once: true }}
+                    transition={{ duration: 1, ease: "easeOut" }}
+                >
+                    <h1>Awards</h1>
+                    <li><b>T. W. Hatcher Scholarship</b> - <i>Virginia Tech, 2025</i></li>
+                    <li><b>Ray A. Gaskins Scholarship</b> - <i>Virginia Tech, 2024 & 2025</i></li>
+                    <li><b>Richard L. and Georgia W. Kimball Scholarship</b> - <i>Virginia Tech, 2024</i></li>
+                </motion.ul>
             </div>
         </section>
     )
