bd623a30552a148e17526125e63f7a9223d86a51
ajlevy246 <ajlevy246@gmail.com>
2025-08-29 15:14:06 -0400

    added project pages

---
+++ src/app/projects/LevyCAS/page.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function LevyCAS() {
+    return (
+        <section className="cas min-h-screen pb-[2000px]">
+            <form className="gui">
+                Operations here!
+            </form>
+        </section>
+    )
+}
---
+++ src/app/projects/PersonalSite/page.tsx
@@ -0,0 +1,5 @@
+export default function PersonalSite() {
+    return (
+        <section />
+    )
+}
