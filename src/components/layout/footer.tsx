547fde17da3385b8b40a025e25a5f55a3ff40bcd
Alex Levy <ajlevy246@gmail.com>
2026-07-24 11:25:55 -0400

    Move components to components/layout/

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,46 @@
+import { FaGithub } from "react-icons/fa";
+import { BiLogoLinkedinSquare } from "react-icons/bi";
+import { CgMail } from "react-icons/cg";
+
+// @ts-ignore: allow side-effect css import without type declarations
+import "./footer.css";
+import { ReactElement } from "react";
+
+type FooterItemProps = {
+  image: ReactElement;
+  href: string;
+};
+
+function FooterItem({ image, href}: FooterItemProps) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`footer-icon transition`}
+    >
+      {image}
+    </a>
+  );
+}
+
+export default function Footer() {
+  return (
+    <div className="hidden sm:flex flex-col p-6 text-white text-center items-center gap-2 fixed bottom-0">
+      <div className="flex flex-row flex-wrap gap-6 items-center justify-center ml-5 mb-3 text-4xl">
+        <FooterItem
+          image={<BiLogoLinkedinSquare />}
+          href="https://www.linkedin.com/in/alexlevy246"
+        />
+        <FooterItem
+          image={<FaGithub />}
+          href="https://github.com/ajlevy246"
+        />
+        <FooterItem
+          image={<CgMail />}
+          href="mailto:ajlevy246@gmail.com"
+        />
+      </div>
+    </div>
+  )
+}
